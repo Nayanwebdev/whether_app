8ef8d3fa4805c53d7c557e578329b742dda85da3
Nayanwebdev <nayanwebdevelopment1@gmail.com>
2023-03-07 14:59:09 +0530

    set mobile height

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 import WeatherApp from "./WeatherApp";
 
 function App() {
+  const doc = document.documentElement
+    doc.style.setProperty('--app-height', `${window.innerHeight}px`)
   return (
     <div className="whether-block-main">
       <WeatherApp />
