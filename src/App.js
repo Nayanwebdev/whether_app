0dbd3729c2b3c34a68ccfdfce67f42d2d42fadaa
Nayanwebdev <nayanwebdevelopment1@gmail.com>
2023-03-07 15:01:28 +0530

    vhfixer

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 import WeatherApp from "./WeatherApp";
 
 function App() {
+
   const doc = document.documentElement
+  const vhFixer = ()=>{
     doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+  }
+
+  vhFixer()
+
+  window.addEventListener('resize', vhFixer)
+  
   return (
     <div className="whether-block-main">
       <WeatherApp />
